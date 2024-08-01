7bf1838fe91c8bed53a8d3b075a6eda2d7584272
Cameron Cooper <cameron.cooper@loyalhealth.com>
2024-08-01 13:36:03 -0400

    add client boilerplate, add socket.io

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,8 +1,36 @@
+import importlib
 from fastapi import FastAPI
-from .websocket import websocket_router
+from socketio import ASGIApp, AsyncServer
 from .routing import api_router
 
+def socketio_mount(
+    app: FastAPI,
+    async_mode: str = "asgi",
+    mount_path: str = "/socket.io/",
+    socketio_path: str = "socket.io",
+    logger: bool = False,
+    engineio_logger: bool = False,
+    cors_allowed_origins="*",
+    **kwargs
+) -> AsyncServer:
+    """Mounts an async SocketIO app over an FastAPI app."""
+
+    sio = AsyncServer(async_mode=async_mode,
+                      cors_allowed_origins=cors_allowed_origins,
+                      logger=logger,
+                      engineio_logger=engineio_logger, **kwargs)
+
+    sio_app = ASGIApp(sio, socketio_path=socketio_path)
+
+    # mount
+    app.add_route(mount_path, route=sio_app, methods=["GET", "POST"])
+    app.add_websocket_route(mount_path, sio_app)
+
+    return sio
+
 app = FastAPI()
+sio = socketio_mount(app, mount_path="/socket.io/", socketio_path="socket.io")
 
 app.include_router(api_router, prefix="/api")
-app.include_router(websocket_router, prefix="/ws")
+
+importlib.import_module("app.websocket")
--- api/app/websocket/__init__.py
+++ api/app/websocket/__init__.py
@@ -1,12 +1,5 @@
-from fastapi import APIRouter, WebSocket
+from app import sio
 
-websocket_router = APIRouter()
-
-@websocket_router.websocket("/")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        
-        # Echo 
-        await websocket.send_text(f"Message text was: {data}")
+@sio.event
+async def connect(sid, data):
+    print(f"Client {sid} connected")
