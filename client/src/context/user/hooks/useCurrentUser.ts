d052a0756893945d5c21edbaed47d51e879daf47
Cameron Cooper <cameron.cooper@loyalhealth.com>
2024-08-03 21:41:02 -0400

    enhance useCurrentUser by storing context value and allowing refreshes

--- client/src/context/user/hooks/useCurrentUser.ts
+++ client/src/context/user/hooks/useCurrentUser.ts
@@ -1,9 +1,24 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserDto } from "../../../models/dto";
 import { UserContext } from "../UserContext";
+import UserApi from "../../../api/UserApi";
 
-export const useCurrentUser = (): UserDto | null => {
+export const useCurrentUser = (): [UserDto | null, CallableFunction] => {
     const { userDetails } = useContext(UserContext);
+    const [cachedDetails, setCachedDetails] = useState<UserDto | null>(
+        userDetails
+    );
 
-    return userDetails;
+    useEffect(() => {
+        setCachedDetails(userDetails);
+    }, [userDetails]);
+
+    return [
+        cachedDetails,
+        () => {
+            UserApi.getCurrentUser().then((user) => {
+                setCachedDetails(user);
+            });
+        },
+    ];
 };
