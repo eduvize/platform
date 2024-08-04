20c197b0dbecad59ab2e682986b1cd7aff86d586
Cameron Cooper <cameron.cooper@loyalhealth.com>
2024-08-03 21:40:02 -0400

    add extra fields to user profile dto

--- client/src/models/dto/UserProfileDto.ts
+++ client/src/models/dto/UserProfileDto.ts
@@ -1,7 +1,19 @@
+export interface UserProgrammingLanguage {
+    name: string;
+    proficiency: number | null;
+}
+
+export interface UserLibrary {
+    name: string;
+    proficiency: number | null;
+}
+
 export interface UserProfileDto {
     first_name: string | null;
     last_name: string | null;
     bio: string | null;
     github_username: string | null;
     avatar_url: string | null;
+    programming_languages: UserProgrammingLanguage[];
+    libraries: UserLibrary[];
 }
