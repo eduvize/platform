5b0c843bac8bfe57559c14c4eb34bcc002757691
Cameron Cooper <cameron.cooper@loyalhealth.com>
2024-08-05 12:20:07 -0400

    add file router with resume scan endpoint

--- api/app/routing/__init__.py
+++ api/app/routing/__init__.py
@@ -6,8 +6,10 @@ from .onboarding_router import router as onboarding_router
 from .user_router import router as user_router
 from .auth_router import router as auth_router
 from .autocomplete_router import router as autocomplete_router
+from .file_router import router as file_router
 
 api_router.include_router(auth_router)
 api_router.include_router(user_router)
 api_router.include_router(onboarding_router)
 api_router.include_router(autocomplete_router)
+api_router.include_router(file_router)
