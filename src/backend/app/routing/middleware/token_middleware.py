87c5d787882e12757b337d6d273c9b754c78675b
Cameron Cooper <cameron.cooper@loyalhealth.com>
2024-08-15 16:59:40 -0400

    add token middleware for getting the access token without validation, make token_validator check the blacklist

--- src/backend/app/routing/middleware/token_middleware.py
+++ src/backend/app/routing/middleware/token_middleware.py
@@ -1,14 +1,19 @@
+from typing import Optional
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 from app.routing.responses import raise_unauthorized
 from app.utilities.jwt import InvalidJWTToken, decode_token
+from app.services.auth_service import TOKEN_BLACKLIST_SET
+from common.cache import is_in_set_with_expiration
 from config import get_token_secret
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-def token_extractor(token: str = Depends(oauth2_scheme)) -> dict:
+def token_validator(
+    token: str = Depends(oauth2_scheme)
+) -> dict:
     """
-    Extracts the JWT information from the request token
+    Extracts the JWT information from the request token while also ensuring it is not blacklisted
 
     Args:
         token (str, optional): The token provided in an HTTP request.
@@ -20,11 +25,32 @@ def token_extractor(token: str = Depends(oauth2_scheme)) -> dict:
         dict: The decoded token information
     """
     try:
-        return decode_token(token, get_token_secret())
+        data = decode_token(token, get_token_secret())
+    
+        if is_in_set_with_expiration(TOKEN_BLACKLIST_SET, token):
+            raise_unauthorized()
+            
+        return data
     except InvalidJWTToken:
         raise_unauthorized()
+        
+def get_access_token(token: str = Depends(oauth2_scheme)) -> Optional[str]:
+    """
+    Extracts the access token from the token in the request
+
+    Args:
+        token (str): The token provided in an HTTP request
+
+    Returns:
+        str: The access token
+    """
     
-def user_id_extractor(token: dict = Depends(token_extractor)) -> str:
+    try:
+        return token
+    except InvalidJWTToken:
+        return None
+    
+def user_id_extractor(token: dict = Depends(token_validator)) -> str:
     """
     Extracts the authorized user's ID from the token in the request
 
