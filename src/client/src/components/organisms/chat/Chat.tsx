e674e3ec1fd80672b4c0669469bc46d4c9bf7464
Cameron Cooper <cameron.cooper@loyalhealth.com>
2024-08-20 17:54:58 -0400

    make Chat organism display active tools, add additional props for tool usage

--- src/client/src/components/organisms/chat/Chat.tsx
+++ src/client/src/components/organisms/chat/Chat.tsx
@@ -1,14 +1,28 @@
-import { useChatMessages } from "@context/chat/hooks";
-import { Input, ScrollArea, Stack } from "@mantine/core";
-import { ChatMessage } from "@molecules";
 import { useEffect, useRef, useState } from "react";
+import { ChatMessage, PendingTool } from "@molecules";
+import {
+    useChatMessages,
+    usePendingTools,
+    useToolResults,
+} from "@context/chat/hooks";
+import { Box, Input, ScrollArea, Stack } from "@mantine/core";
 
 interface ChatProps {
     height: string;
+    toolDescriptionMap?: Record<string, string>;
+    onTool?: (name: string, data: any) => void;
 }
 
-export const Chat = ({ height }: ChatProps) => {
+/**
+ * The chat component is a top-level component that directly ties into the chat context to handle the sending, receiving and display of messages
+ * in a common format. Tools are handled by consumers of the chat component, where the toolDescriptionMap is used to provide
+ * a user-friendly description of the tool being processed, and onTool is used to handle the results of tools.
+ */
+export const Chat = ({ height, toolDescriptionMap, onTool }: ChatProps) => {
     const viewport = useRef<HTMLDivElement>(null);
+    const isAtBottomRef = useRef(true);
+    const pendingToolNames = usePendingTools();
+    const toolResults = useToolResults();
     const [messages, sendMessage] = useChatMessages();
     const [message, setMessage] = useState("");
 
@@ -21,7 +35,21 @@ export const Chat = ({ height }: ChatProps) => {
 
     useEffect(() => {
         handleScrollToBottom();
-    }, [messages]);
+    }, [pendingToolNames.length, messages.length]);
+
+    useEffect(() => {
+        if (isAtBottomRef.current) {
+            handleScrollToBottom();
+        }
+    }, [messages.map((x) => x.content)]);
+
+    useEffect(() => {
+        if (!toolResults) return;
+
+        for (const [name, data] of Object.entries(toolResults)) {
+            onTool?.(name, data);
+        }
+    }, [toolResults]);
 
     return (
         <Stack gap={0}>
@@ -29,12 +57,31 @@ export const Chat = ({ height }: ChatProps) => {
                 viewportRef={viewport}
                 h={height}
                 scrollbars="y"
+                onScroll={() => {
+                    isAtBottomRef.current =
+                        viewport.current!.scrollTop +
+                            viewport.current!.clientHeight >=
+                        viewport.current!.scrollHeight;
+                }}
             >
                 <Stack gap="xl">
                     {messages.map((message) => (
                         <ChatMessage {...message} />
                     ))}
                 </Stack>
+
+                {pendingToolNames.length > 0 && (
+                    <Box mt="md">
+                        {pendingToolNames.map((toolName) => (
+                            <PendingTool
+                                name={
+                                    toolDescriptionMap?.[toolName] ??
+                                    "Working some magic..."
+                                }
+                            />
+                        ))}
+                    </Box>
+                )}
             </ScrollArea.Autosize>
 
             <Input
