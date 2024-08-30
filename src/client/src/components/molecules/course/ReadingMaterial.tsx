c70d1768e7b24676f09de06b2335572782c204f8
Cameron Cooper <cameron.cooper@loyalhealth.com>
2024-08-30 12:48:36 -0400

    add some custom styling rules to ReadingMaterial component

--- src/client/src/components/molecules/course/ReadingMaterial.tsx
+++ src/client/src/components/molecules/course/ReadingMaterial.tsx
@@ -1,9 +1,10 @@
 import Markdown from "react-markdown";
+import classes from "./ReadingMaterial.module.css";
 
 interface ReadingMaterialProps {
     children: string;
 }
 
 export const ReadingMaterial = ({ children }: ReadingMaterialProps) => {
-    return <Markdown>{children}</Markdown>;
+    return <Markdown className={classes.markdown}>{children}</Markdown>;
 };
