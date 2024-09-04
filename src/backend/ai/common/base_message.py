7815e6fd59b742ea85a663d991db81eeef6cec33
Cameron Cooper <cameron.cooper@loyalhealth.com>
2024-09-03 20:21:38 -0400

    use correct type hint for tool_calls

--- src/backend/ai/common/base_message.py
+++ src/backend/ai/common/base_message.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from typing import List, Optional
-from .base_tool import BaseToolCall
+from .base_tool import BaseToolCallWithResult
 
 class ChatRole(Enum):
     USER = 1
@@ -11,14 +11,14 @@ class BaseChatMessage:
     role: ChatRole
     png_images: List[bytes]
     message: Optional[str]
-    tool_calls: List[BaseToolCall] = []
+    tool_calls: List[BaseToolCallWithResult] = []
     
     def __init__(
         self, 
         role: ChatRole, 
         message: Optional[str], 
         png_images: List[bytes] = [],
-        tool_calls: List[BaseToolCall] = []
+        tool_calls: List[BaseToolCallWithResult] = []
     ):
         self.role = role
         self.message = message
