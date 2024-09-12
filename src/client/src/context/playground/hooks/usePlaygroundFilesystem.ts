861e8bed7444356b00860a17df19ca795bffe534
Cameron Cooper <cameron.cooper@loyalhealth.com>
2024-09-12 16:54:36 -0400

    add close file support to usePlaygroundFilesystem hook

--- src/client/src/context/playground/hooks/usePlaygroundFilesystem.ts
+++ src/client/src/context/playground/hooks/usePlaygroundFilesystem.ts
@@ -6,6 +6,7 @@ interface UsePlaygroundFilesystemReturn {
     entries: FilesystemEntry[];
     openFiles: string[];
     openFile: (path: string) => void;
+    closeFile: (path: string) => void;
     createFile: (path: string) => void;
     createDirectory: (path: string) => void;
 }
@@ -13,6 +14,7 @@ interface UsePlaygroundFilesystemReturn {
 export const usePlaygroundFilesystem = (): UsePlaygroundFilesystemReturn => {
     const create = useContextSelector(PlaygroundContext, (v) => v.create);
     const openFile = useContextSelector(PlaygroundContext, (v) => v.openFile);
+    const closeFile = useContextSelector(PlaygroundContext, (v) => v.closeFile);
     const openFiles = useContextSelector(PlaygroundContext, (v) => v.openFiles);
     const entries =
         useContextSelector(
@@ -24,6 +26,7 @@ export const usePlaygroundFilesystem = (): UsePlaygroundFilesystemReturn => {
         entries,
         openFiles: Object.keys(openFiles),
         openFile,
+        closeFile,
         createFile: (path: string) => create("file", path),
         createDirectory: (path: string) => create("directory", path),
     };
