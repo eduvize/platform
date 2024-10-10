7fde808df675afb1154df7f8e1cb9ef783aede3c
Cameron Cooper <cameron.cooper@loyalhealth.com>
2024-10-09 22:02:18 -0400

    make resume scan async

--- src/backend/app/routing/file_router.py
+++ src/backend/app/routing/file_router.py
@@ -17,6 +17,6 @@ async def get_resume_details(file: UploadFile = File(...)):
     if not file.content_type == "application/pdf":
         return Response(status_code=status.HTTP_400_BAD_REQUEST, content="Only PDF files are supported")
     
-    images = get_images_from_pdf_bytes(await file.read())
+    images = await get_images_from_pdf_bytes(await file.read())
     
     return await ResumeScannerPrompt().get_profile_data(images)
