6b76bbd2562f83f7efb5b8f7239e048dd23bc7d0
Cameron Cooper <cameron.cooper@loyalhealth.com>
2024-10-09 21:59:05 -0400

    making email sending async

--- src/backend/common/email/__init__.py
+++ src/backend/common/email/__init__.py
@@ -1,16 +1,16 @@
 import logging
 from config import get_mailgun_key, get_email_noreply_address
-import requests
+import aiohttp
 
 logger = logging.getLogger(__name__)
 
-def send_email(
+async def send_email(
     to: str, 
     subject: str, 
     content: str
 ):
     """
-    Sends an email to a recipient using Amazon Simple Email Service (SES)
+    Sends an email to a recipient using Mailgun API asynchronously
 
     Args:
         to (str): The recipient's email address
@@ -21,15 +21,17 @@ def send_email(
     access_key = get_mailgun_key()
     source_address = get_email_noreply_address()
     
-    response = requests.post(
-        "https://api.mailgun.net/v3/mail.eduvize.dev/messages",
-        auth=("api", access_key),
-        data={
-            "from": f"Eduvize <{source_address}>",
-            "to": [to],
-            "subject": subject,
-            "html": content
-        }
-    )
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            "https://api.mailgun.net/v3/mail.eduvize.dev/messages",
+            auth=aiohttp.BasicAuth("api", access_key),
+            data={
+                "from": f"Eduvize <{source_address}>",
+                "to": [to],
+                "subject": subject,
+                "html": content
+            }
+        ) as response:
+            status_code = response.status
     
-    logger.info(f"Email sent to {to} with status code {response.status_code}")
+    logger.info(f"Email sent to {to} with status code {status_code}")
